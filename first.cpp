e911668bb62695b9c30b6f3c13de5c23b185e5d8
rambo78-sensei <rohitsbelgaum@gmail.com>
2022-08-26 19:51:25 +0530

    Main

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum;
+cout<<"Enter the number";  
+cin>>n;
+sum=n*(n+1)/2;
+cout<<"The sum of two numbers is"<<sum;
+    return 0;
+}
